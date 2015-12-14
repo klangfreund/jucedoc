4f52e4f65e98a7c222658eedeb81d889db77d06f
Samuel Gaehwiler <sg@klangfreund.com>
2015-12-14 21:01:36 +0100

    Automatic update.

--- update.sh
+++ update.sh
@@ -19,4 +19,10 @@ rsync --progress -avz --delete $juceDirectory/doxygen/doc .
 # Commit and upload
 git add --all .
 git commit -m "Automatic update."
+
+# Needed to make 'git push' work from within a cron job
+# (Source: http://stackoverflow.com/questions/16578520/git-push-not-working-inside-a-cron-job )
+export SSH_AGENT_PID=`ps -a | grep ssh-agent | grep -o -e [0-9][0-9][0-9][0-9]`
+export SSH_AUTH_SOCK=`find /tmp/ -path '*keyring-*' -name '*ssh*' -print 2>/dev/null`
+
 git push
