92fab6195048893129bd0dc1617ad9b19d1e0d4a
Samuel Gaehwiler <sg@klangfreund.com>
2015-12-14 21:06:34 +0100

    Automatic update.

--- update.sh
+++ update.sh
@@ -21,8 +21,6 @@ git add --all .
 git commit -m "Automatic update."
 
 # Needed to make 'git push' work from within a cron job
-# (Source: http://stackoverflow.com/questions/16578520/git-push-not-working-inside-a-cron-job )
-export SSH_AGENT_PID=`ps -a | grep ssh-agent | grep -o -e [0-9][0-9][0-9][0-9]`
-export SSH_AUTH_SOCK=`find /tmp/ -path '*keyring-*' -name '*ssh*' -print 2>/dev/null`
+HOME=/home/juce
 
 git push
