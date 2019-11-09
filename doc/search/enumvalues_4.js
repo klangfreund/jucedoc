f4dabc4b530cba630eb4022a50e1782b881149a8
Samuel Gaehwiler <sg@klangfreund.com>
2019-11-09 20:00:23 +0100

    Automatic update.

--- doc/search/enumvalues_4.js
+++ doc/search/enumvalues_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['east',['east',['../structBlock_1_1ConnectionPort.html#adb5cd70e80bd854e5a40436a99d003eaa320381dbf7cd2f31c04baa6b36e1c682',1,'Block::ConnectionPort']]],
+  ['editablecolour',['editableColour',['../classColourSelector.html#a42bcef80614375bb812096797d7439f4a6eb75ad1de52e34bb795431b849ead3a',1,'ColourSelector']]],
   ['editableonpalette',['editableOnPalette',['../classToolbarItemComponent.html#ad499f9f48a5149685ab8da86244fd4e4a440b4d0912bbbaf54955a30814e5bd6a',1,'ToolbarItemComponent']]],
   ['editableontoolbar',['editableOnToolbar',['../classToolbarItemComponent.html#ad499f9f48a5149685ab8da86244fd4e4a33d140eb9c5d5bc9e4ce6cd838dc30aa',1,'ToolbarItemComponent']]],
   ['editingmodeoutlinecolourid',['editingModeOutlineColourId',['../classToolbar.html#ae89655c1fc755d7648eedcefcb620bc9a783c2d3c7bee40ce820b9fc1b8450d6f',1,'Toolbar']]],
